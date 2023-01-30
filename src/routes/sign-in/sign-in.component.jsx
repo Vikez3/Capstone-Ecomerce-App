bcba95036304f692a1df4fc676ccb89865c70ba8
Viko-53 <vikobranevski93@gmail.com>
2023-01-30 23:35:56 +0100

    created sign up comp button comp

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,16 +3,19 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 export default function SignIn() {
   const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       sign-in.component
       <button onClick={logGoogleUser}>Sign with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 }
